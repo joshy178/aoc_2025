d0a769a4a3cffa4ee4f6363d5f9135f16673fc4a
joshy178 <joshieserva@gmail.com>
2025-12-06 10:51:43 -0700

    1.2 complete

--- 1/1.py
+++ 1/1.py
@@ -1,6 +1,8 @@
 
+num_times_zero = 0
 
 def left_move( position, amt_to_move ):
+    rollover = amt_to_move // 100
     amt_to_move = amt_to_move % 100
     val = position - amt_to_move
 
@@ -8,10 +10,14 @@ def left_move( position, amt_to_move ):
         # plus one to account for rollover
         # example -2 means at pos 98
         # 99 + -2 + 1 == 98
-        return 99 + 1 + val 
-    return val
+        new_position = 99 + 1 + val
+        if new_position != 0 and position != 0:
+            rollover += 1
+        return new_position, rollover
+    return val, rollover
 
 def right_move( position, amt_to_move ):
+    rollover = amt_to_move // 100
     amt_to_move = amt_to_move % 100
     val = position + amt_to_move
 
@@ -20,27 +26,33 @@ def right_move( position, amt_to_move ):
         # 100 means at pos 0
         # 0 == 100 - 99 - 1
         diff = val - 99
-        return diff - 1
-    return val
+        new_position = diff - 1
+        if new_position != 0 and position != 0:
+            rollover += 1
+        return new_position, rollover
+    return val, rollover
     
 
 position = 50
 print(f'The dial starts by pointing at {position}.')
-num_times_zero = 0
-for line in open('input.txt', 'r').readlines():
+for line in open('input2.txt', 'r').readlines():
     line = line.strip()
     direction = line[0]
     amt_to_move = int(line[1:])
+    rollover = 0
     if direction == 'L':
-        position = left_move(position=position, amt_to_move=amt_to_move)
+        position, rollover = left_move(position=position, amt_to_move=amt_to_move)
     else:
         assert direction == 'R'
-        position = right_move(position=position, amt_to_move=amt_to_move)
+        position, rollover = right_move(position=position, amt_to_move=amt_to_move)
     extra_str = ''
     if position == 0:
         extra_str = '------------'
         num_times_zero += 1
     print(f'The dial is rotated {line} to point at {position}.{extra_str}')
+    print(f'it points at zero {rollover} time(s)')
+    num_times_zero += rollover
+
     if position < 0:
         break
 
