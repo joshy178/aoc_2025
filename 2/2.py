9e565f20bdb19adee85179614dc88758d9b366e6
joshy178 <joshieserva@gmail.com>
2025-12-06 11:33:35 -0700

    2.2 complete

--- 2/2.py
+++ 2/2.py
@@ -5,18 +5,28 @@ for thing in ranges:
     temp = thing.split('-')
     bottom = int(temp[0])
     top = int(temp[1])
-    for i in range(bottom, top + 1):
-        text  = str(i)
+    patterns = {}
+    for num in range(bottom, top + 1):
+        # print('------------------------------')
+        # print(num)
+        text  = str(num)
         # print(text)
         length = len(text)
-        if (length % 2 ) != 0:
-            continue
-        left = text[:length//2]
-        right = text[length//2:]
-        # print(f'left {left}')
-        # print(f'right {right}')
-        if left == right:
-            print(f'{text} is invalid')
-            id_sum += i
+        patterns = {}
+        is_invalid = False
+        for i in range(1, (length//2) + 1):
+            sub_text = text[0:i]
+            sub_text_len = len(sub_text)
+            if length % sub_text_len != 0:
+                continue
+            if ( sub_text * (length // sub_text_len) ) == text:
+                is_invalid = True
+                break
+
+        if is_invalid:
+            print(f'id {num} is invalid')
+            id_sum += num
+        # print('------------------------------')
+
     # break
 print(f'id sum {id_sum}')
