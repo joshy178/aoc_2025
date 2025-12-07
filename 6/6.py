cb2f03dd8a176c769b852f1f0217a0b38a657c7a
joshy178 <joshieserva@gmail.com>
2025-12-07 09:13:52 -0700

    6.2 done

--- 6/6.py
+++ 6/6.py
@@ -34,57 +34,93 @@
 
 num_map = []
 ops = []
-for line in open('input.txt', 'r').readlines():
-    line = line.strip()
-    if line[0] in ['+', '*']:
-        ops = line.split()
-    else:
-        nums = [int(x) for x in line.split()]
+for line in open('input2.txt', 'r').readlines():
+    line = line.replace('\n', '')
+    row = []
+    for char in line:
+        row.append(char)
+    num_map.append(row)
+transposed = []
+while len(num_map[0]) > 0:
+    new_row = []
+    for i, row in enumerate(num_map):
+        new_row.append(row.pop(-1))
+    transposed.append(new_row)
 
-        num_map.append(nums)
-
-
-# print(num_map)
+# for row in transposed:
+    # print(row)
 grand_total = 0
-totals = []
-for j in range(0, len(num_map[0])):
-    op_str = ''
-    hit_zero = False
-    new_nums = []
-    for i in range(0, len(num_map)):
-        new_nums.append(num_map[i][j])
-        # print(new_nums)
-    new_nums.sort()
-    new_nums.reverse()
-    first_op = True
-    total = 0
-    final_nums = [0] * len(new_nums)
-    while new_nums[0] > 0:
-        print(new_nums)
-        newer_nums = []
-        for i, num in enumerate(new_nums):
-            # largest number has no digits left
-            # bail
-            if i == 0 and num == 0:
-                break
-            newer_num = num // 10
-            newer_nums.append(newer_num)
-            num = num % 10
-            if first_op:
-                first_op = False
-                total = num
-                op_str += str(num)    
-            if ops[j] == '+':
+nums = []
+for row in transposed:
+    num_str = ''.join(row)
+    if num_str.strip() == '':
+        nums = []
+        continue
+    if num_str.endswith(('+', '*')):
+        op = num_str[-1]
+        num = int(num_str[:-1])
+        nums.append(num)
+        calc_str = op.join([str(x) for x in nums])
+        if op == '+':
+            total = nums.pop()
+            for num in nums:
                 total += num
-            else:
-                assert ops[j] == '*'
+            calc_str += f'={total}'
+            print(calc_str)
+        if op == '*':
+            total = nums.pop()
+            for num in nums:
                 total *= num
-            op_str += ops[j] + str(num)
-        new_nums = newer_nums
-    grand_total += total
-    print(f'{op_str}={total}')
+            calc_str += f'={total}'
+            print(calc_str)
+        grand_total += total
+    else:
+        nums.append(int(num_str))
+
+print(f'grand total {grand_total}')
 
 
-# print(num_map)
-print(f'Grand Total: {grand_total}')
+# # print(num_map)
+# grand_total = 0
+# totals = []
+# for j in range(0, len(num_map[0])):
+#     op_str = ''
+#     hit_zero = False
+#     new_nums = []
+#     for i in range(0, len(num_map)):
+#         new_nums.append(num_map[i][j])
+#         # print(new_nums)
+#     new_nums.sort()
+#     new_nums.reverse()
+#     first_op = True
+#     total = 0
+#     final_nums = [0] * len(new_nums)
+#     while new_nums[0] > 0:
+#         print(new_nums)
+#         newer_nums = []
+#         for i, num in enumerate(new_nums):
+#             # largest number has no digits left
+#             # bail
+#             if i == 0 and num == 0:
+#                 break
+#             newer_num = num // 10
+#             newer_nums.append(newer_num)
+#             num = num % 10
+#             if first_op:
+#                 first_op = False
+#                 total = num
+#                 op_str += str(num)    
+#             if ops[j] == '+':
+#                 total += num
+#             else:
+#                 assert ops[j] == '*'
+#                 total *= num
+#             op_str += ops[j] + str(num)
+#         new_nums = newer_nums
+#     grand_total += total
+#     print(f'{op_str}={total}')
+
+
+# # print(num_map)
+# print(f'Grand Total: {grand_total}')
 
